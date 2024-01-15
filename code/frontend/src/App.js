e3fa9e7bf760056a3937cd3fc59223bdc0de933c
kavane39 <ethkav03@gmail.com>
2024-01-15 11:23:41 +0000

    Updates to frontend

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import Navbar from './Navbar';
-import LoginForm from './components/LoginForm';
+import Register from './components/Register';
+import Login from './components/Login';
+import Users from './components/Users';
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <LoginForm />
+    <div className='app'>
+      <Navbar pathname={useLocation().pathname} />
+      <Routes>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Users />}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+<div className="App">
+      <Navbar />
+      <Users />
+      <Register />
+    </div>
