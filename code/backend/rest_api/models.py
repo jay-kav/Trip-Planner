69d89d322c16d7462d9c249092a5c334eeedce3f
kavane39 <ethkav03@gmail.com>
2024-01-17 15:14:15 +0000

    Updated models.py and admin.py for new models

--- code/backend/rest_api/models.py
+++ code/backend/rest_api/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
-class UserProfile(models.Model):
-    userID = models.OneToOneField(User, on_delete=models.CASCADE)
-
 class Trip(models.Model):
     id = models.AutoField(primary_key=True)
     Owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -14,7 +10,7 @@ class Trip(models.Model):
     StartDate = models.DateField()
     EndDate = models.DateField() 
     Itineraries = models.ManyToManyField('Itinerary')
-    Members = models.ManyToManyField(UserProfile)
+    Members = models.ManyToManyField(User)
     Created = models.DateTimeField(auto_now_add=True)
     Activities = ArrayField(models.CharField(max_length=255))  # Array of strings for Activities done s
 
